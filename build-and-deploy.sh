a6a7b62d19913368f7e10b7da674d4581d6b48c9
Julien Pivotto <roidelapluie@inuits.eu>
2016-02-12 20:59:41 +0100

    foo

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -15,8 +15,8 @@ git config user.name "Travis"
 git config user.email "packaginglove@roidelapluie.be"
 git add .
 git commit -m "Deploy to GitHub Pages"
-echo "deploying"
 
+echo ${GH_URL}
 
 git push -f --quiet "https://${GH_TOKEN}@${GH_URL}" master:gh-pages > /dev/null 2>&1
 
