60d500776cf1b38fad68bb4b7ba7e911b41f9e3e
Julien Pivotto <roidelapluie@inuits.eu>
2016-02-12 20:36:51 +0100

    Build and deploy

---
+++ build-and-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+rm -rf _book||true
+
+gitbook build
+
+[[ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]]
+
+cd _book
+git init .
+git config user.name "Travis"
+git config user.email "packaginglove@roidelapluie.be"
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+
+git push -f --quiet "https://${GH_TOKEN}@${GH_URL}" master:gh-pages > /dev/null 2>&1
+
+
